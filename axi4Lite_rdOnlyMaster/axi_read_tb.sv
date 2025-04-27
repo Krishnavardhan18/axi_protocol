6f556459a2de6857bdbd99bd33e703f4aa9ba765
Krishnavardhan18 <chkrishnavardhan18@gmail.com>
2025-04-27 19:37:13 +0530

    Comments re updated

--- axi4Lite_rdOnlyMaster/axi_read_tb.sv
+++ axi4Lite_rdOnlyMaster/axi_read_tb.sv
@@ -5,26 +5,20 @@ module axi_read_tb;
     parameter ADDR_WIDTH = 32;
     parameter DATA_WIDTH = 32;
 
-    // Clock and Reset
     logic clk, rst;
-
-    // Master-Side Signals
     logic start, done;
     logic [ADDR_WIDTH-1:0]   ARADDR;
     logic                    ARVALID;
     logic                    RREADY;
     logic [DATA_WIDTH-1:0]   READ_DATA;
 
-    // Slave-Side Signals
     logic                    ARREADY;
     logic                    RVALID;
     logic [DATA_WIDTH-1:0]   RDATA;
     logic [1:0]              RRESP;
 
-    // Clock Generation
     always #5 clk = ~clk;
 
-    // DUT: Read-Only AXI4-Lite Master
     master_fsm #(
         .ADDR_WIDTH(ADDR_WIDTH),
         .DATA_WIDTH(DATA_WIDTH)
@@ -42,8 +36,6 @@ module axi_read_tb;
         .RRESP(RRESP),
         .RVALID(RVALID)
     );
-
-    // Dummy AXI4-Lite Slave
     dummy_slave #(
         .ADDR_WIDTH(ADDR_WIDTH),
         .DATA_WIDTH(DATA_WIDTH)
@@ -51,7 +43,7 @@ module axi_read_tb;
         .clk(clk),
         .rst(rst),
         .start(start),
-        .done(), // not used
+        .done(), //not used
         .ARADDR(ARADDR),
         .ARVALID(ARVALID),
         .RREADY(RREADY),
@@ -61,26 +53,21 @@ module axi_read_tb;
         .RRESP(RRESP)
     );
 
-    // Test Sequence
     initial begin
         $dumpfile("waveform.vcd");
         $dumpvars(0, axi_read_tb);
 
-        // Initialize
         clk = 0;
         rst = 0;
         start = 0;
 
-        // Reset Pulse
         #10 rst = 1;
         #10 rst = 0;
         #10 rst = 1;
 
-        // Start AXI Read
         #10 start = 1;
         #20 start = 0;
 
-        // Wait for DONE
         wait(done);
 
         $display("[TB] Read Completed. Data Received = %h", READ_DATA);
